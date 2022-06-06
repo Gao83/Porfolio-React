821649bac0efa51576fe6a971c93fc606e817a17
Isaac Miralles <isaacmiralles@gmail.com>
2022-06-06 18:09:54 +0200

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+import CTA from './CTA'
+
+const Header = () => {
+    return (
+        <header>
+         
+
+        </header>
+    )
+}
+
+export default Header
