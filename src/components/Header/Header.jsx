67b362d6c0a6109e68cec6f519cafe7c9eca42de
Isaac Miralles <isaacmiralles@gmail.com>
2022-06-06 19:05:25 +0200

    working on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import './Header.css'
 import CTA from './CTA'
+import ME from '../../assets/Isaac.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
     return (
         <header>
-         
+            <div className="container header__container">
 
+                <h5>Hello! I'm</h5>
+                <h1>Isaac Miralles</h1>
+                <h5 className="text-light">Full Stack Developer</h5>
+                <CTA />
+
+                <div className="me">
+                    <img src={ME} alt="me" />
+                </div>
+                <a href="#contact" className='scroll__down'>Scroll Down</a>
+                <HeaderSocials />
+            </div>
         </header>
     )
 }
---
+++ src/components/Header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BsLinkedin } from 'react-icons/bs'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+          <a href="https://www.linkedin.com/in/isaac-miralles/" target="_blank"><BsLinkedin /></a>
+          <a href="https://github.com/Gao83" target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
